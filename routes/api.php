c6d6ee374b33a995e1dc0dfb274938e8fbeac96d
Demitri-Ansss <ansoricahgreges@gmail.com>
2026-02-15 12:47:33 +0700

    feat: define API routes for To-Do list management and user authentication.

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working']);
+});
+
 Route::get('/user', function (Request $request) {
     return response()->json([
         'name' => $request->user()->name,
