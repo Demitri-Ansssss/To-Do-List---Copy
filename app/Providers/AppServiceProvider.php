85b56db9e772152443092d2547b9bc51bbd983b4
Demitri-Ansss <ansoricahgreges@gmail.com>
2026-02-15 14:01:07 +0700

    feat: Implement AppServiceProvider to define API and web routes and configure Vercel-specific storage and HTTPS settings.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,20 +19,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {        
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
         if (isset($_ENV['VERCEL'])) {
             $path = '/tmp/storage/framework/views';
             if (!is_dir($path)) {
                 mkdir($path, 0777, true);
             }
             $this->app->useStoragePath('/tmp/storage');
-            $this->routes(function () {
-                Route::middleware('api')
-                    ->prefix('api')
-                    ->group(base_path('routes/api.php'));
-
-                Route::middleware('web')
-                    ->group(base_path('routes/web.php'));
-            });
         }
         // Only force HTTPS when actually deployed on Vercel, not on local development
         if (env('VERCEL', false)) {
