a300a63d8fb60fb5bd51eb4746e8b9402f374a9a
Demitri-Ansss <ansoricahgreges@gmail.com>
2026-02-15 14:00:02 +0700

    feat: Add Vercel-specific configuration to AppServiceProvider for storage paths and HTTPS.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,13 +18,21 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
+    {        
         if (isset($_ENV['VERCEL'])) {
             $path = '/tmp/storage/framework/views';
             if (!is_dir($path)) {
                 mkdir($path, 0777, true);
             }
             $this->app->useStoragePath('/tmp/storage');
+            $this->routes(function () {
+                Route::middleware('api')
+                    ->prefix('api')
+                    ->group(base_path('routes/api.php'));
+
+                Route::middleware('web')
+                    ->group(base_path('routes/web.php'));
+            });
         }
         // Only force HTTPS when actually deployed on Vercel, not on local development
         if (env('VERCEL', false)) {
